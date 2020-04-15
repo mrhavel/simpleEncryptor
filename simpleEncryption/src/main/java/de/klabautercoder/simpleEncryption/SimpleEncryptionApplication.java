d6b9393394cab2c031d598770b1390d3ef2efe26
Han Wavel <32564691+mrhavel@users.noreply.github.com>
2020-04-15 07:27:42 +0200

    Add first Spring Project Files

---
+++ simpleEncryption/src/main/java/de/klabautercoder/simpleEncryption/SimpleEncryptionApplication.java
@@ -0,0 +1,13 @@
+package de.klabautercoder.simpleEncryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEncryptionApplication.class, args);
+	}
+
+}
---
+++ simpleEncryption/src/test/java/de/klabautercoder/simpleEncryption/SimpleEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.klabautercoder.simpleEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
